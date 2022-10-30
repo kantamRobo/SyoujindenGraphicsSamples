4d3f36cfcb8435ea2b4f73bd96cade0f4c6af8d2
anazi6j <hattendosummer@yahoo.co.jp>
2022-10-30 19:16:45 +0900

    明日デバッグしてみる

--- Assets/Scripts/Particlesystemformoving.cs
+++ Assets/Scripts/Particlesystemformoving.cs
@@ -2,6 +2,7 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 
     public class Particlesystemformoving : MonoBehaviour
@@ -43,8 +44,27 @@ using UnityEngine;
 
         if (isRunning)
         {
-            particlesystem.startLifetime = running_particlelifetime;
+
+            //パーティクルシステムのレンダラーにアタッチされているマテリアルの
+            //光を強める
+            
+            var particles = new ParticleSystem.Particle[particlesystem.main.maxParticles];
+            //寿命を迎えてないパーティクルの速度を上げる
+            //注意:パーティクルシステムでこれから出てくるパーティクルの速度を上げるのではなく、
+            //生存しているパーティクルの速度を上げる
+            int aliveParticlesCount = particlesystem.GetParticles(particles);
             particlesystemrenderer.material.SetColor("_EmissionColor", runningemmisioncolor);
+            particlesystem.startLifetime = running_particlelifetime;
+            for (int i=0; i < aliveParticlesCount;i++)
+            {
+                particles[i].velocity = particles[i].velocity.normalized * (particles[i].velocity.magnitude + Time.deltaTime);
+                
+            }
+
+            particlesystem.SetParticles(particles, aliveParticlesCount);
+
+           
+            
         }
 
         //パーティクルの寿命が来たら
